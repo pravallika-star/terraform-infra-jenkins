c98d1cb7454328de223f2432dc8c59911bed3278
pravallika-star <pravallika.r.juturu@gmail.com>
2025-04-06 11:02:18 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  description = "aws region"
+  type = string
+}
+
+variable "instance_type" {
+  description = "instance type"
+  type = string
+}
+
+variable "ami_id" {
+  description = "ami value"
+  type = string
+}
