faf86be3e499b1196c268d50342a4dd381c8d637
pravallika-star <pravallika.r.juturu@gmail.com>
2025-04-06 11:40:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,21 @@
 provider "aws" {
   region = var.aws_region
 }
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical (Ubuntu) official
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 resource "aws_instance" "prava" {
   ami           = var.ami_id
   instance_type = var.instance_type
